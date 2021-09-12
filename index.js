32de11738d9a56b54f892d9f8969aa4ce8d54eab
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2021-09-12 16:01:50 +0100

    e

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function installContent(files) {
     && (!file.endsWith(path.sep))));
 
   const instructions = filtered.map(file => {
-    if(modFile.name.endsWith("_P")){
+    if(file.name.endsWith("_P")){
     return {
       type: 'copy',
       source: file,
