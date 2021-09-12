f4919c162df65711d6e6bee47cd40f5b14ab0cfa
bananaturtlesandwich <71292624+bananaturtlesandwich@users.noreply.github.com>
2021-09-12 15:13:20 +0100

    Registered a mod installer

--- game-bluefire/index.js
+++ game-bluefire/index.js
@@ -23,68 +23,70 @@ function findGame() {
 }
 
 function prepareForModding(discovery) {
-  if(file.name.indexOf('_P')>-1){
     return fs.ensureDirAsync(path.join(discovery.path, 'Blue Fire', 'Content', 'Paks', '~mods'));
-  }
-  else{
-    return fs.ensureDirAsync(path.join(discovery.path, 'Blue Fire', 'Content', 'Paks', '~mods','LogicMods'));
-  }
 }
 
+function testSupportedContent(files, gameId) {
+  // Make sure we're able to support this mod.
+  let supported = (gameId === GAME_ID) &&
+    (files.find(file => path.extname(file).toLowerCase() === MOD_FILE_EXT) !== undefined);
+
+  return Promise.resolve({
+    supported,
+    requiredFiles: [],
+  });
+}
+
+function installContent(files) {
+  // The .pak file is expected to always be positioned in the mods directory we're going to disregard anything placed outside the root.
+  const modFile = files.find(file => path.extname(file).toLowerCase() === MOD_FILE_EXT);
+  const idx = modFile.indexOf(path.basename(modFile));
+  const rootPath = path.dirname(modFile);
+  
+  // Remove directories and anything that isn't in the rootPath.
+  const filtered = files.filter(file => 
+    ((file.indexOf(rootPath) !== -1) 
+    && (!file.endsWith(path.sep))));
+
+  const instructions = filtered.map(file => {
+    return {
+      type: 'copy',
+      source: file,
+      destination: path.join(file.substr(idx)),
+    };
+  });
+
+  return Promise.resolve({ instructions });
+}
 
 function main(context) {
-	//This is the main function Vortex will run when detecting the game extension. 
-	if(file.name.indexOf('_P')>-1){
-    context.registerGame({
-      id: GAME_ID,
-      name: 'Blue Fire',
-      mergeMods: true,
-      queryPath: findGame,
-      supportedTools: [],
-      queryModPath: () => 'Blue Fire/Content/Paks/~mods',
-      logo: 'gameart.jpg',
-      executable: () => 'PROA34.exe',
-      requiredFiles: [
-        'PROA34.exe',
-        'Blue Fire/Binaries/Win64/PROA34-Win64-Shipping.exe'
-      ],
-      setup: prepareForModding,
-      environment: {
-        SteamAPPId: STEAMAPP_ID,
-      },
-      details: {
-        steamAppId: STEAMAPP_ID,
-        gogAppId: GOGAPP_ID,
-      },
-    });
-  }
-  else{
-    context.registerGame({
-      id: GAME_ID,
-      name: 'Blue Fire',
-      mergeMods: true,
-      queryPath: findGame,
-      supportedTools: [],
-      queryModPath: () => 'Blue Fire/Content/Paks/LogicMods',
-      logo: 'gameart.jpg',
-      executable: () => 'PROA34.exe',
-      requiredFiles: [
-        'PROA34.exe',
-        'Blue Fire/Binaries/Win64/PROA34-Win64-Shipping.exe'
-      ],
-      setup: prepareForModding,
-      environment: {
-        SteamAPPId: STEAMAPP_ID,
-      },
-      details: {
-        steamAppId: STEAMAPP_ID,
-        gogAppId: GOGAPP_ID,
-      },
-    });
-  }
-  
-	return true
+  //This is the main function Vortex will run when detecting the game extension. 
+  context.registerGame({
+    id: GAME_ID,
+    name: 'Blue Fire',
+    mergeMods: true,
+    queryPath: findGame,
+    supportedTools: [],
+    queryModPath: () => 'Blue Fire/Content/Paks/~mods',
+    logo: 'gameart.jpg',
+    executable: () => 'PROA34.exe',
+    requiredFiles: [
+      'PROA34.exe',
+      'Blue Fire/Binaries/Win64/PROA34-Win64-Shipping.exe'
+    ],
+    setup: prepareForModding,
+    environment: {
+      SteamAPPId: STEAMAPP_ID,
+    },
+    details: {
+      steamAppId: STEAMAPP_ID,
+      gogAppId: GOGAPP_ID,
+    },
+  });
+  context.registerInstaller('bluefire-mod', 25, testSupportedContent, installContent);
+  return true
 }
+
 
 module.exports = {
     default: main,
